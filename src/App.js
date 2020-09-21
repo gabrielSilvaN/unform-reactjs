3b8b37e0e1776619a402b83505562bac2aee4d38
Gabriel <gabriel_silva@discente.ufg.br>
2020-09-21 11:17:44 -0300

    finalizaçao do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+
+import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+
+import * as Yup from 'yup';
+
+import Input from './components/Form/Input';
+
 import './App.css';
 
 function App() {
+
+
+  const formRef = useRef(null);
+
+  // objeto que contém os valores iniciais do form (opcional)
+  const initialData = {
+    email: "gabs_pdc@hotmail.com",
+    address: {
+      street: "Rua ED-5"
+    }
+  }
+
+  async function handleSubmit(data, { reset }) {
+
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string().email('Digite um e-mail válido').required('O e-mail é obrigatório'),
+        address: Yup.object().shape({
+          city: Yup.string().min(3, 'No mínimo 3 caracteres').required('A cidade é obrigatória')
+        })
+      });
+
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+
+      formRef.current.setErrors({});
+
+      console.log(data);
+
+      reset();
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message
+        })
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
+
+  }
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      formRef.current.setData({
+        name: "Gabriel",
+        email: "gabs_pdc@hotmail.com"
+      })
+    }, 2000)
+  }, []);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Hello World</h1>
+
+      {/* é possível utilizar o initalData(dados estáticos para o form) bem como dados vindos de uma api */}
+      <Form ref={formRef} initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input name="email" />
+        <Input type="password" name="password" />
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+          <Input name="number" />
+        </Scope>
+        <button type="submit">Enviar</button>
+      </Form>
+
     </div>
+
   );
 }
 
---
+++ src/components/Form/Input.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+function Input({ name, ...rest }) {
+
+   const inputRef = useRef(null);
+   const { fieldName, registerField, defaultValue, error } = useField(name);
+
+
+   useEffect(() => {
+
+      registerField({
+         name: fieldName,
+         ref: inputRef.current,
+         path: 'value'
+      })
+
+   }, [fieldName, registerField]);
+
+   return (
+      <div>
+         <input ref={inputRef} defaultValue={defaultValue} {...rest} />
+         {error && <span style={{color: "#f00"}}>{error}</span>}
+      </div>
+   );
+}
+
+export default Input;
